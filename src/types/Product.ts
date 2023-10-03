adb2f84a4b7ba2f3f96cceb8a3b2eaf8823e0d87
egorlagunovich <egorlagunovich@gmail.com>
2023-10-03 14:47:51 +0200

    redux configured

---
+++ src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string;
+  slug: string;
+  image: string;
+  category: string;
+  brand: string;
+  price: string;
+  countInStock: number;
+  description: string;
+  rating: number;
+  numReviews: number;
+};
